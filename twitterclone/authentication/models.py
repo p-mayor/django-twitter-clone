ba7173716a02d7546f9b3c68a6c3541881bf4938
petem14 <petem14@gmail.com>
2019-12-03 11:35:32 -0500

    add twitter user model

--- twitterclone/authentication/models.py
+++ twitterclone/authentication/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class TwitterUser(models.Model):
+    user=models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    name = models.CharField(max_length=20)
+    bio = models.TextField("Bio", null=True, blank=True)
+    tweet_count = models.IntegerField()
+    following_count = models.IntegerField()
+    def __str__(self):
+        return self.name
