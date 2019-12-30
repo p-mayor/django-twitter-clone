3b120a836fd081b6d1b94a5a4b78fff42a271e37
petem14 <petem14@gmail.com>
2019-12-29 21:50:30 -0500

    add 140 char limit to tweet body

--- twitterclone/tweets/models.py
+++ twitterclone/tweets/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from twitterclone.twitterusers.models import TwitterUser
+from django.core.validators import MaxLengthValidator
 # Create your models here.
 
 class Tweet(models.Model):
     twitter_user = models.ForeignKey(TwitterUser, on_delete=models.CASCADE)
-    body = models.TextField("Body", null=True, blank=True)
+    body = models.TextField("Body", null=True, blank=True, validators=[MaxLengthValidator(140)])
     time = models.DateTimeField(auto_now_add=True)
